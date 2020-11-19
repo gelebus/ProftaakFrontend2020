88f029c96eb53aa0217dc1731145b80e2469f4f3
Robbert Lengton <robbert.lengton@hotmail.nl>
2020-11-19 23:48:16 +0100

    Title changes accordingly

--- Js/Main.js
+++ Js/Main.js
@@ -72,7 +72,8 @@ function ConnectToSocket(token) {
 
 function StartGame(data){
   $('body').load('Game.html', ()=>{
-    history.pushState('data', '', 'Game.html');
+    history.pushState('data', 'Battleships - Game', 'Game.html');
+    document.title = 'Battleships - Game';
     $('span#LobbyId').text(data.game_code);
     $(`#Player${data.self_index + 1}`).text(data.players[data.self_index].name);
   });
