969d3b9586ddd055a2189a63f7a276730de6ffbb
Robbert Lengton <robbert.lengton@hotmail.nl>
2020-11-26 09:45:50 +0100

    Socket functionalities in main.js moved to socket.js

--- Js/Main.js
+++ Js/Main.js
@@ -60,21 +60,3 @@ function JoinGame(){
     }
   }
 }
-
-function ConnectToSocket(token) {
-  const socket = io('ws://localhost:3000');
-  socket.emit('token', token);
-  socket.on('game_info', response =>{
-    console.log(response);
-    StartGame(response);
-  });
-}
-
-function StartGame(data){
-  $('body').load('Game.html', ()=>{
-    history.pushState('data', 'Battleships - Game', 'Game.html');
-    document.title = 'Battleships - Game';
-    $('span#LobbyId').text(data.game_code);
-    $(`#Player${data.self_index + 1}`).text(data.players[data.self_index].name);
-  });
-}
