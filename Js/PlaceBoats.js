6f288cf9a9e2fbce1f90fae54c2168bfed0c0568
Robbert Lengton <robbert.lengton@hotmail.nl>
2020-11-28 22:29:41 +0100

    Player is able to select ship

---
+++ Js/PlaceBoats.js
@@ -0,0 +1,15 @@
+function SelectShip(e) {
+  let availableShips = document.getElementsByClassName('AvailableShip');
+
+  for (let i = 0; i < availableShips.length; i++) {
+    if(e != availableShips[i]){
+      availableShips[i].classList.remove('Active');
+    }
+  }
+
+  if(e.classList.contains('Active')){
+    e.classList.remove('Active');
+  }else{
+    e.classList.add('Active');
+  }
+}
