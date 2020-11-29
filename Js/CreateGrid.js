90c8081bf01c11778f7d0c84e4a7972580869a98
Robbert Lengton <robbert.lengton@hotmail.nl>
2020-11-29 18:08:24 +0100

    Update highlight function

--- Js/CreateGrid.js
+++ Js/CreateGrid.js
@@ -1,41 +1,41 @@
-function CreateGrid(gridId){
+function CreateGrid(gridID){
   for (let row = 0; row < 11; row++) {
-    let divRow = CreateDivRow(row);
-    document.getElementById(gridId).appendChild(divRow);
+    let divRow = CreateDivRow(gridID, row);
+    document.getElementById(gridID).appendChild(divRow);
   }
 }
 
-function CreateDivRow(rowNumber){
+function CreateDivRow(gridID, rowNumber){
   let divRow = document.createElement('div');
   divRow.classList.add('row');
 
   for (let column = 0; column < 11; column++) {
-    divRow.appendChild(CreateDivColumn(rowNumber, column));
+    divRow.appendChild(CreateDivColumn(gridID, rowNumber, column));
   }
 
   return divRow;
 }
 
-function CreateDivColumn(rowNumber, columnNumber){
+function CreateDivColumn(gridID, rowNumber, columnNumber){
   let lettersA2J = ['X','A','B','C','D','E','F','G','H','I','J'];
 
   let divColumn = document.createElement('div');
 
   if(rowNumber == 0){
-    let id = `col_${lettersA2J[columnNumber]}`;
+    let id = `${gridID}_col_${lettersA2J[columnNumber]}`;
     let indicator = lettersA2J[columnNumber];
     divColumn = CreateGridIndicator(divColumn, id, indicator);
   }
 
   if(columnNumber != 0){
     if(rowNumber != 0){
-      divColumn.addEventListener("mouseover", HighlightRowColumn);
+      divColumn.addEventListener("mouseover", AddHighlight);
       divColumn.classList.add('grid-btn');
-      divColumn.id = `${rowNumber}_${lettersA2J[columnNumber]}`;
+      divColumn.id = `${gridID}_${rowNumber}_${lettersA2J[columnNumber]}`;
     }
   }else{
     if(rowNumber != 0){
-      let id = `row_${rowNumber}`;
+      let id = `${gridID}_row_${rowNumber}`;
       let indicator = rowNumber;
       divColumn = CreateGridIndicator(divColumn, id, indicator);
     }else{
--- Js/GridIndicator.js
+++ Js/GridIndicator.js
@@ -5,11 +5,12 @@ function RemoveHighlight(){
   }
 }
 
-function HighlightRowColumn(e){
+function AddHighlight(e){
   RemoveHighlight();
 
-  let row = e.target.id.split('_')[0];
-  let col = e.target.id.split('_')[1];
-  document.getElementById(`row_${row}`).classList.add('highlight');
-  document.getElementById(`col_${col}`).classList.add('highlight');
+  let targetSplit = e.target.id.split('_');
+  let rowID = `${targetSplit[0]}_row_${targetSplit[1]}`;
+  let colID = `${targetSplit[0]}_col_${targetSplit[2]}`;
+  document.getElementById(rowID).classList.add('highlight');
+  document.getElementById(colID).classList.add('highlight');
 }
