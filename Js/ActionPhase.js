ca1622a1b03193caf6faa80d4b85a09d79e0bd20
Freaxy <freek.vlier@gmail.com>
2020-12-17 11:28:45 +0100

    Player is able to select cell

--- Js/ActionPhase.js
+++ Js/ActionPhase.js
@@ -1,5 +1,16 @@
 function AttackShip(e){
-  alert('You attack cell: ' + e.target.id);
+  let cell = e.target;
+  let gridId = cell.parentElement.parentElement.id;
+  console.log('You attack cell: ' + e.target.id);
+  let cells = document.getElementsByClassName("grid-btn");
+
+  for (let i = 0; i < cells.length; i++) {
+    cells[i].classList.remove('cell-selected');
+  }
+
+  if (gridId == 'EnemyGrid') {
+    cell.classList.add('cell-selected');
+  }
 }
 
 function AddLayoutToGrid(gridID, shipLayout){
